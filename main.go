d026af5cfa3d0d33b09b9f29d584fe649fdc9ec1
Pedro.Marçal <pedro.h.silva.marcal@gmail.com>
2022-10-30 01:21:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/PedroSMarcal/hackaton2022/configs"
-	"github.com/PedroSMarcal/hackaton2022/repository/database"
+	"github.com/PedroSMarcal/hackaton2022/controllers"
 )
 
 func main() {
 	configs.GetEnvironmentVariables()
 
-	database.StartDb()
+	// database.StartDb()
 
-	// controllers.Run()
+	controllers.Run()
 }
